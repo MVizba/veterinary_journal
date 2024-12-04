bd1a3db5a639a418585c889fe1e31e1c7be2f256
Marius Vizbras <marius.viz@gmail.com>
2024-12-04 14:31:05 +0200

    Examination CRUD

--- src/Controller/ExaminationController.php
+++ src/Controller/ExaminationController.php
@@ -9,28 +9,76 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-class ExaminationController extends AbstractController
+#[Route('/examination')]
+final class ExaminationController extends AbstractController
 {
+    #[Route(name: 'app_examination_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $examinations = $entityManager
+            ->getRepository(Examination::class)
+            ->findAll();
 
-    #[Route('/add-examination', name: 'add_examination')]
-    #[isGranted('ROLE_ADMIN')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+        return $this->render('examination/index.html.twig', [
+            'examinations' => $examinations,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_examination_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $examination = new Examination();
         $form = $this->createForm(ExaminationType::class, $examination);
-
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($examination);
-            $em->flush();
 
-            $this->addFlash('success', 'examination added successfully.');
-            return $this->redirectToRoute('dashboard');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($examination);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_examination_index', [], Response::HTTP_SEE_OTHER);
         }
-        return $this->render('examination/add.html.twig', [
-            'form' => $form->createView(),
+
+        return $this->render('examination/new.html.twig', [
+            'examination' => $examination,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_examination_show', methods: ['GET'])]
+    public function show(Examination $examination): Response
+    {
+        return $this->render('examination/show.html.twig', [
+            'examination' => $examination,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_examination_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Examination $examination, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ExaminationType::class, $examination);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_examination_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('examination/edit.html.twig', [
+            'examination' => $examination,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_examination_delete', methods: ['POST'])]
+    public function delete(Request $request, Examination $examination, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$examination->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($examination);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_examination_index', [], Response::HTTP_SEE_OTHER);
     }
 }
